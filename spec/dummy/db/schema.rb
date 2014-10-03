ff54c0eaf1dfad44e6fe178f61f225a8cf385dfa
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-10-03 11:07:44 -0500

    configure for travis

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20140829235335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", force: true do |t|
+  create_table "event_capture_events", force: true do |t|
     t.datetime "emitted_at"
     t.datetime "recorded_at"
     t.text     "payload"
